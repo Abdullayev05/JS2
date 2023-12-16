2c8e7cde993cf15abeb4c371b886194d3bf2745e
Abdullayev05 <abdullayevmahammadaga@gmail.com>
2023-12-16 22:25:09 +0400

    js2

---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+// console.log(Math.random)
+// function getRandomInt(max) {
+//     return Math.floor(Math.random() * max);
+//   }
+//   console.log(getRandomInt(3));
+//   console.log(getRandomInt(1));
+//   console.log(Math.random());
+
+
+// var name = prompt("")
+// var key = 19;
+// switch(key) {
+//   case 18:
+//     console.log ("18");
+//     break;
+//   case 19:
+//     console.log("19");
+//     break;
+//   default:
+//     console.log("hecne");
+//     break
+// }
+
+
+// var sentence = prompt("Cumle yaz")
+// var mode = prompt("Reqem yaz")
+// if (sentence.length < 2) {
+//       console.log("2")
+//   }
+// alert ("")
